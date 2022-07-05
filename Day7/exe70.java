f66e726ffd8fca5e238626397870488d1f7ba3f5
gabriel <gaamdal557@gmail.com>
2022-07-05 20:53:14 -0300

    operações básicas4

---
+++ Day7/exe70.java
@@ -0,0 +1,14 @@
+package Day7;
+
+public class exe70 {
+
+	public static void main(String[]args) {
+		 String str1 = "Python";    
+			String str2 = "Tutorial"; 
+			
+			if(str1.length() >= str2.length())
+				System.out.println( str2+str1+str2);
+			else
+			System.out.println(str1+str2+str1);	
+	}
+}
