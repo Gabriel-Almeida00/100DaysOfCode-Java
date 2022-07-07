77d17735cd05d17b56c3be55adcb495e4dfbab41
gabriel <gaamdal557@gmail.com>
2022-07-07 19:08:07 -0300

    arraye string

---
+++ Day9/exe85.java
@@ -0,0 +1,9 @@
+package Day9;
+
+public class exe85 {
+
+	public static void main(String[]args) {
+		 String string1 = "Hello how are you?";
+		    System.out.println(string1.startsWith("Hello"));
+	}
+}
---
+++ Day9/exe89.java
@@ -0,0 +1,9 @@
+package Day9;
+
+public class exe89 {
+
+	public static void main(String[]args) {
+		System.out.println("System security interface:");
+	    System.out.println(System.getSecurityManager());
+	}
+}
