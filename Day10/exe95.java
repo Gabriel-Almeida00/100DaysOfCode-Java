3ed80f43ce12e66aee8790f55a58494dd90fbf27
gabriel <gaamdal557@gmail.com>
2022-07-08 08:48:26 -0300

    Arrays

---
+++ Day10/exe95.java
@@ -0,0 +1,14 @@
+package Day10;
+import java.util.Arrays;
+
+public class exe95 {
+
+	public static void main(String[]args) {
+		 int n= 5;
+			String[] arr_string = new String[n];
+			
+		    for(int i = 0; i < n; i++)
+		        arr_string[i] = String.valueOf(i);                                  
+			 System.out.println("New Array: "+Arrays.toString(arr_string)); 
+	}
+}
