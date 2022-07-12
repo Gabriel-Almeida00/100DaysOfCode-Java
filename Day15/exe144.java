73e6da2945d89f5b7fe6e996a653ba5db79d32ec
gabriel <gaamdal557@gmail.com>
2022-07-12 14:12:52 -0300

    String

---
+++ Day15/exe144.java
@@ -0,0 +1,28 @@
+package Day15;
+
+import java.util.Arrays;
+
+public class exe144 {
+	 public static void main(String[] args) {
+			int x = 6;
+			int [] nums = {1,4,6,7,6,2};
+			System.out.println("Original array: "+Arrays.toString(nums));
+	        System.out.println("The length of the new array is: " + removeElement(nums, x));
+		}
+
+	 public static int removeElement(int[] nums, int elem) {
+	      int length = nums.length; 
+	      if(length==0) return 0; 
+	      int i=0; 
+	      for(int j=0; j<length; j++)
+	      {
+	        if(nums[j]!=elem)
+	        {
+	          nums[i]=nums[j];
+	          i++; 
+	        }
+	      }
+	      if(i<length) nums[i]='\0';
+	      return i; 
+	    }
+}
