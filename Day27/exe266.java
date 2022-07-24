2f424b0bfaaf691d8bb934a422faec806fc682c6
gabriel <gaamdal557@gmail.com>
2022-07-24 20:32:06 -0300

    tipos de dados e estruturas de decisão

---
+++ Day27/exe266.java
@@ -0,0 +1,25 @@
+package Day27;
+
+import java.util.Scanner;
+
+public class exe266 {
+	 public static void main(String[] args)
+	    {
+	        Scanner in = new Scanner(System.in);
+	        System.out.print("Input number: ");
+	        int input = in.nextInt();
+
+	        if (input > 0)
+	        {
+	            System.out.println("Number is positive");
+	        }
+	        else if (input < 0)
+	        {
+	            System.out.println("Number is negative");
+	        }
+	        else
+	        {
+	            System.out.println("Number is zero");
+	        }
+	    }
+}
