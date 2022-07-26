ccf885fdf20b0e6a5f3978a9947f5ac1dc0b5166
gabriel <gaamdal557@gmail.com>
2022-07-26 11:03:01 -0300

    estrura de decisão e repetição

---
+++ Day30/exe293.java
@@ -0,0 +1,25 @@
+package Day30;
+
+import java.util.Scanner;
+
+public class exe293 {
+	public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int n = in.nextInt();
+
+        if (n > 0)
+        {
+            System.out.println("Number is positive");
+        }
+        else if (n < 0)
+        {
+            System.out.println("Number is negative");
+        }
+        else
+        {
+            System.out.println("Number is zero");
+        }
+    }
+}
