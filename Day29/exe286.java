31f912f33974eb839943a54c6e4013d2779fe47b
gabriel <gaamdal557@gmail.com>
2022-07-25 21:03:46 -0300

    estruras de repetição

---
+++ Day29/exe286.java
@@ -0,0 +1,23 @@
+package Day29;
+
+import java.util.Scanner;
+
+public class exe286 {
+	public static void main(String[] args)
+	 {
+	   int numberOfRows;
+	   System.out.print("Input number of rows : ");
+	   Scanner in = new Scanner(System.in);
+			    numberOfRows = in.nextInt();
+	   int number = 1;
+	   for (int row = 1; row <= numberOfRows; row++)
+	    {
+	   for (int column = 1; column <= row; column++)
+	     {
+	       System.out.print(number + " ");
+	       number++;
+	     }
+	     System.out.println();
+	    }
+	 }
+}
