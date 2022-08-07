495d4b2a97fab9feb8742bcdfe9287da88cc3519
gabriel <gaamdal557@gmail.com>
2022-08-06 23:13:17 -0300

    string

---
+++ Day40/exe392.java
@@ -0,0 +1,15 @@
+package Day40;
+
+public class exe392 {
+	 public static void main(String[] args)
+	    {
+	        String str = "Python Exercises.";
+
+	        // Get the hash code for the above string.
+	        int hash_code = str.hashCode();
+
+	        // Display the hash code.
+	        System.out.println("The hash for " + str +
+	            " is " + hash_code);
+	    }
+}
