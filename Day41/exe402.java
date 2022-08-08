86f590ac8689967a0bfb5430ed4339ca2f5dde84
gabriel <gaamdal557@gmail.com>
2022-08-07 21:44:51 -0300

    string

---
+++ Day41/exe402.java
@@ -0,0 +1,14 @@
+package Day41;
+
+public class exe402 {
+	 public static void main(String[] args)
+	    {
+	        String str = "Java Exercises.";
+
+	        // Convert the above string to a char array.
+	        char[] arr = str.toCharArray();
+
+	        // Display the contents of the char array.
+	        System.out.println(arr);
+	    }
+}
