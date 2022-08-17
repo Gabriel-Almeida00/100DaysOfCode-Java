4c0974d52812215ff37ab3d223653b9e8d6de5e8
gabriel <gaamdal557@gmail.com>
2022-08-17 14:43:34 -0300

    method

---
+++ Day45/exe449.java
@@ -0,0 +1,14 @@
+package Day45;
+
+public class exe449 {
+	 public static void main(String[] args) {
+	        print_Chars('(', 'z', 20);
+	    }
+	 public static void print_Chars(char char1, char char2, int n) {
+	        for (int ctr = 1; char1 <= char2; ctr++, char1++) {
+	            System.out.print(char1 + " ");
+	            if (ctr % n == 0) System.out.println("");
+	        }
+			System.out.print("\n");
+	    }
+}
