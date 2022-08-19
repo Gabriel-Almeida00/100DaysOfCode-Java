7f5cec678172b44e2970c0e672833f25a9f5b2cc
gabriel <gaamdal557@gmail.com>
2022-08-19 17:11:01 -0300

    numbers and method

---
+++ Day47/exe465.java
@@ -0,0 +1,15 @@
+package Day47;
+
+import java.util.Scanner;
+
+public class exe465 {
+	 public static void main( String args[] ){ 
+		    Scanner sc = new Scanner( System.in ); 
+	        System.out.print("Input the starting number of the range: "); 
+	        int rsnum = sc.nextInt(); 
+	        System.out.print("Input the ending number of the range: "); 
+	        int renum = sc.nextInt(); 
+		    int random_num = rsnum + (int)(Math.random() * ((renum - rsnum) + 1));
+		    System.out.println(random_num);
+	     }  
+}
